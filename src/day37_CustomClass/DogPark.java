df7523dfa1095c73cad78f974cd9f8bd2f5af640
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-29 12:23:59 -0400

    day37

---
+++ src/day37_CustomClass/DogPark.java
@@ -0,0 +1,29 @@
+package day37_CustomClass;
+
+public class DogPark {
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.name = "Kangal";
+        dog1.gender = "Female";
+        dog1.breed = "Kangal";
+        dog1.age = 5;
+        dog1.size = "large";
+        dog1.color = "yellowish brown";
+
+
+        System.out.println( dog1.name );
+        System.out.println(dog1.gender);
+        System.out.println(dog1.breed);
+        System.out.println(dog1.size);
+        System.out.println(dog1.age);
+        System.out.println(dog1.color);
+
+        Dog dog2 = new Dog();
+
+
+
+
+    }
+
+}
