cd26a158076354461d627b50cac721e41444a697
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-02 16:02:37 -0400

    day38

--- src/day38_CustomClass/offerTask/MJobOffers.java
+++ src/day38_CustomClass/offerTask/MJobOffers.java
@@ -1,4 +1,21 @@
 package day38_CustomClass.offerTask;
 
 public class MJobOffers {
+
+    public static void main(String[] args) {
+        Offer offer1 = new Offer();
+        Offer offer2 = new Offer();
+        Offer offer3 = new Offer();
+        Offer offer4 = new Offer();
+        Offer offer5 = new Offer();
+        Offer offer6 = new Offer();
+        Offer offer7 = new Offer();
+
+        offer1.setInfo("CA", "Los Angles", "SDET", 120000, true, false, false, false);
+        offer2.setInfo("TX", "Austin", "Software Developer", 130000, true, true, true, false);
+        
+
+
+    }
+
 }
