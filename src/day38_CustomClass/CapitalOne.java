d5a374a79c2164937600a5bae80e9ccaa2455aa9
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-02 15:15:01 -0400

    day38

--- src/day38_CustomClass/CapitalOne.java
+++ src/day38_CustomClass/CapitalOne.java
@@ -18,8 +18,15 @@ public class CapitalOne {
         account1.deposit(100000);
         account1.checkBalance();
 
+        System.out.println("---------------------------------------------");
 
+        BankAccount account2 = new BankAccount();
+        account2.setInfo("Ahmad", 789456123);
 
+        account2.deposit(100000);
+
+        account1.checkBalance();
+        account2.checkBalance();
 
     }
 
