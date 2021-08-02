f28045f913280eda5bb162b3e314ba333ebf1fb2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-02 14:57:44 -0400

    day38

--- src/day38_CustomClass/BankAccount.java
+++ src/day38_CustomClass/BankAccount.java
@@ -21,6 +21,25 @@ public class BankAccount {
         balance += amount;
     }
 
+    public void withdraw(double amount){
+        if(amount <= 0){
+            System.out.println("Withdrawing amount can not be negative or zero");
+            return;
+        }
+
+        if(balance <= 0){
+            System.out.println("Insufficient balance");
+            return;
+        }
+
+        if(amount > balance){
+            balance -= 35; // 35$ penalty fee will be added
+        }
+
+        System.out.println("Withdrawing $"+amount+" from the account "+accountNumber);
+        balance -= amount;
+
+    }
 
 
 
@@ -39,7 +58,7 @@ BankAccount
         requirements:
             1. user should be able to deposit money into their account
             2. user should be able to withdraw money from their account
-                2.1 if the withdrawing account is greater than available balance, 35$ penalty charge will be added
+                2.1 if the withdrawing amount is greater than available balance, 35$ penalty charge will be added
                 2.2 if the balance is less than or equal to 0, user should not be able to withdraw money
             3. user should be able to see their balance
  */
