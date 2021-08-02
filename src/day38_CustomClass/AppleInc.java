e9d24bb3d5596ca3ee0b66c6cdd0ee162d874bfa
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-02 13:11:57 -0400

    day38

--- src/day38_CustomClass/AppleInc.java
+++ src/day38_CustomClass/AppleInc.java
@@ -1,6 +1,7 @@
 package day38_CustomClass;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AppleInc {
 
@@ -19,17 +20,53 @@ public class AppleInc {
         employee4.setInfo("Fhilipp",'M',21,"SDET",120000);
 
         Employee employee5 = new Employee();
-        employee5.setInfo("Ella", 'F', 13,"QA",110000);
+        employee5.setInfo("Ella", 'F', 14,"QA",110000);
 
         Employee employee6 = new Employee();
         employee6.setInfo("Ahmet",'M',15, "QA", 150000);
 
         Employee employee7 = new Employee();
-        employee7.setInfo("Sophie", 'F', 13, "SDET", 120000);
+        employee7.setInfo("Sophie", 'F', 16, "SDET", 120000);
 
 
+        Employee[] arr = {employee1, employee2, employee3, employee4, employee5, employee6, employee7};
         ArrayList<Employee> employees = new ArrayList<>();
+        employees.addAll( Arrays.asList(arr ) );
+        double budget = 0;
 
+        for(Employee each  : employees){
+            //System.out.println( each.name +" : "+each.salary);
+            System.out.println(each);
+            budget += each.salary;
+        }
+
+        System.out.println("budget = " + budget);
+
+        System.out.println("--------------------------------------------");
+        // remove all the emploees thata re making more than 130K
+        ArrayList<Employee> list1 = new ArrayList<>( employees);
+        list1.removeIf( employee ->  employee.salary >= 130000  );
+
+        for (Employee each : list1) {
+            System.out.println(each);
+        }
+
+        System.out.println("-------------------------------");
+        // remove all the employees that are not SDET
+        ArrayList<Employee> list2 = new ArrayList<>(employees);
+        list2.removeIf( p -> !p.jobTitle.equals("SDET") );
+        /*
+        for (Employee each : employees) {
+            if(each.jobTitle.equals("SDET") ){
+                list2.add(each);
+            }
+        }
+         */
+
+
+        for (Employee each : list2) {
+            System.out.println(each);
+        }
 
 
 
