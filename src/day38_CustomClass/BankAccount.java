3422b06db4faf34111ace6de653c4de7ab096b49
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-02 14:46:48 -0400

    day38

---
+++ src/day38_CustomClass/BankAccount.java
@@ -0,0 +1,45 @@
+package day38_CustomClass;
+
+public class BankAccount {
+
+    public String accountHolder;
+    public long accountNumber;
+    public double balance;
+
+    public void setInfo(String accountHolder, long accountNumber){
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(double amount){
+        if(amount <= 0){
+            System.out.println("Depositing amount can not be negative or zero");
+            return;
+        }
+
+        System.out.println("Depositing "+amount+" to the account "+ accountNumber);
+        balance += amount;
+    }
+
+
+    
+
+
+
+}
+
+/*
+BankAccount
+        attributes/data that can have are:
+                1. AccountHolder, 2. AccountNumber, 3. Balance
+
+        Actions:
+            1. CheckBalance,   2. deposit,  3. withdraw
+
+        requirements:
+            1. user should be able to deposit money into their account
+            2. user should be able to withdraw money from their account
+                2.1 if the withdrawing account is greater than available balance, 35$ penalty charge will be added
+                2.2 if the balance is less than or equal to 0, user should not be able to withdraw money
+            3. user should be able to see their balance
+ */
