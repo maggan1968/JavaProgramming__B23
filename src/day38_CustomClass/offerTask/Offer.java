a4fa35b65511e0ac6df6700c4c4a76e864d283ad
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-02 15:37:59 -0400

    day38

--- src/day38_CustomClass/offerTask/Offer.java
+++ src/day38_CustomClass/offerTask/Offer.java
@@ -1,4 +1,29 @@
 package day38_CustomClass.offerTask;
 
 public class Offer {
+
+    public String state, city, jobTitle;
+    public double salary;
+    public boolean hasBenefits, isWFH, hasPTO;
+
+    public void setInfo(String state, String city, String jobTitle, double salary, boolean hasBenefits, boolean isWFH, boolean hasPTO) {
+        this.state = state;
+        this.city = city;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        this.hasBenefits = hasBenefits;
+        this.isWFH = isWFH;
+        this.hasPTO = hasPTO;
+    }
+
+
+
 }
+
+/*
+ Attributes:
+	       state, city, hasBenefit(boolean), salary, WFH(boolean), JobTitle
+
+	    Actions:
+	        SetInfo, toString
+ */
