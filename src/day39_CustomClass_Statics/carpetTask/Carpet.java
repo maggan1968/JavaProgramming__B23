64a3b5068cab07fd8104abf631b76bd438421676
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-03 10:10:57 -0400

    day39

---
+++ src/day39_CustomClass_Statics/carpetTask/Carpet.java
@@ -0,0 +1,20 @@
+package day39_CustomClass_Statics.carpetTask;
+
+public class Carpet {
+}
+
+/*
+create a custom class for the Carpet class that should contain the following:
+                instance variables:
+                        width, length, unitPrice, isPersian (boolean)
+
+                instance methods:
+                        customOrder(): sets the carpet' width, length, unitprice, & isParsian
+                        calcCost(): should be able to caculate the total cost of the carpet and return it as double
+                        toString(): should be able to display all the info of the carpet including the total cost of the carpet as calculated by calcCost()
+
+            total price of carpet = (width*length)*unitprice
+
+            if the carpet is persian  carpet, add 200$ to the totalPrice
+
+ */
