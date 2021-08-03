ba7cb2b0bedb8e03ad4b950287935da968ce4439
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-03 10:29:54 -0400

    day39

--- src/day39_CustomClass_Statics/carpetTask/Carpet.java
+++ src/day39_CustomClass_Statics/carpetTask/Carpet.java
@@ -1,6 +1,23 @@
 package day39_CustomClass_Statics.carpetTask;
 
 public class Carpet {
+
+    public double width, length, unitPrice;
+    public boolean isPersian;
+
+    public void customOrder(double width, double length, double unitPrice, boolean isPersian){
+       this.width = width;
+       this.length = length;
+       this.unitPrice = unitPrice;
+       this.isPersian = isPersian;
+    }
+
+    public double calcCost(){
+        double totalPrice = (width*length) * unitPrice;
+        return  (isPersian)? totalPrice+200 : totalPrice;
+    }
+
+
 }
 
 /*
