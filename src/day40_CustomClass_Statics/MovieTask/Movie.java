f01b3dda8ec52084d4f415aaa32d16013ea1c2e6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-05 11:41:26 -0400

    day40

--- src/day40_CustomClass_Statics/MovieTask/Movie.java
+++ src/day40_CustomClass_Statics/MovieTask/Movie.java
@@ -2,13 +2,44 @@ package day40_CustomClass_Statics.MovieTask;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Movie {
+
     public String title, country, genre, director;
     public LocalDate releaseDate;
     public ArrayList<String> casts = new ArrayList<>();
 
+    public void setInfo(String title, String country, String genre, String director, LocalDate releaseDate) {
+        this.title = title;
+        this.country = country;
+        this.genre = genre;
+        this.director = director;
+        this.releaseDate = releaseDate;
+    }
 
+    public void addCast(String cast){
+           casts.add(cast);
+    }
+
+    public void addCast(String[] casts){
+            this.casts.addAll( Arrays.asList(casts) );
+    }
+
+    public void addCast(ArrayList<String> casts){
+         this.casts.addAll( casts );
+    }
+
+    public String toString() {
+        return "Movie{" +
+                "title= '" + title + '\'' +
+                ", country= '" + country + '\'' +
+                ", genre= '" + genre + '\'' +
+                ", director= '" + director + '\'' +
+                ", releaseDate= " + releaseDate +
+                ", total number of casts= " + casts.size() +
+                '}';
+    }
 }
 /*
     Attributes:
