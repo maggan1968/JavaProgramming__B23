78aa11a9dd3165120d0b45a8a25127a84ebc4d8a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-05 11:44:59 -0400

    day40

--- src/day40_CustomClass_Statics/MovieTask/Movie.java
+++ src/day40_CustomClass_Statics/MovieTask/Movie.java
@@ -1,6 +1,7 @@
 package day40_CustomClass_Statics.MovieTask;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -30,13 +31,14 @@ public class Movie {
          this.casts.addAll( casts );
     }
 
-    public String toString() {
+    public String toString() { // January/01/2021 Tuesday
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM/dd/YYYY EEEE");
         return "Movie{" +
                 "title= '" + title + '\'' +
                 ", country= '" + country + '\'' +
                 ", genre= '" + genre + '\'' +
                 ", director= '" + director + '\'' +
-                ", releaseDate= " + releaseDate +
+                ", releaseDate= " + releaseDate.format(df) +
                 ", total number of casts= " + casts.size() +
                 '}';
     }
