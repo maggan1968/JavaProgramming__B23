3979dbba08b47720169b946215c12a16a8d049f0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-05 12:18:36 -0400

    day40

--- src/day40_CustomClass_Statics/Test.java
+++ src/day40_CustomClass_Statics/Test.java
@@ -5,6 +5,9 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println( StaticBlocks.list );
+        StaticBlocks.list.set(0, "Z");
+
+        System.out.println( StaticBlocks.list );
 
 
     }
