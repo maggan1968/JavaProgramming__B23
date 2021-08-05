78e1ec07d35f1a294befb12fa792d2cc34f2dd3d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-05 12:53:19 -0400

    day40

---
+++ src/day40_CustomClass_Statics/ExecutionOfStaticBlock.java
@@ -0,0 +1,23 @@
+package day40_CustomClass_Statics;
+
+public class ExecutionOfStaticBlock {
+
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+
+    }
+
+    static{
+
+    }
+
+    public static void method1(){
+        System.out.println("Method 1");
+    }
+
+
+
+
+
+
+}
