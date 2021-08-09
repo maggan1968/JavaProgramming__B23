7441c051cd58a006358664d052726636b89445f6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-09 16:18:11 -0400

    day41

--- src/day41_CustomClass_Constructor/ScrumTask/BankOfAmerica.java
+++ src/day41_CustomClass_Constructor/ScrumTask/BankOfAmerica.java
@@ -8,10 +8,62 @@ public class BankOfAmerica {
         Tester tester2 = new Tester("Abbos", 'M', "SM", 11, 115000);
         Tester tester3 = new Tester("Selda", 'F', "QE", 12, 120000);
 
-        System.out.println(tester1);
-        System.out.println(tester2);
-        System.out.println(tester3);
-       // System.out.println(tester1.isEmployeed);
+        Tester[] testers = {tester2, tester3};
+
+
+        Developer developer1 = new Developer("Zorana", 'F', "Software Developer",13, 125000);
+        Developer developer2 = new Developer("Mehmet", 'M', "Java Developer", 14, 130000);
+        Developer developer3 = new Developer("Yuliia", 'F', "Software Developer", 15, 125005);
+        Developer developer4 = new Developer("Yasin", 'M', "Java Developer", 16, 122000);
+
+        Developer[] developers = {developer2, developer3, developer4};
+
+        ScrumTeam scrum1 = new ScrumTeam("Shazia", "Fhilipp", "Aysu");
+        scrum1.hireTester(tester1);
+        scrum1.hireDeveloper(developer1);
+        scrum1.hireTester(testers);
+        scrum1.hireDeveloper(developers);
+
+        scrum1.hireTester(  new Tester("Tugba", 'F', "SDET", 9, 118000)  );
+
+        System.out.println(scrum1);
+
+        double budget = 0;
+
+        for(Tester eachTester  : scrum1.testers){
+            budget += eachTester.salary;
+        }
+
+        for (Developer eachDeveloper : scrum1.developers) {
+              budget +=  eachDeveloper.salary ;
+        }
+
+        System.out.println("budget = " + budget);
+
+        ScrumTeam scrum2 =new ScrumTeam("", "", ""); // add 5 testers & 6 developers
+        ScrumTeam scrum3 =new ScrumTeam("", "", ""); // add 2 testers & 4 developers
+
+        ScrumTeam[] scrumTeams ={scrum1, scrum2, scrum3};
+
+        /*
+        for (ScrumTeam eachScrum : scrumTeams) {
+
+            for (Tester tester : eachScrum.testers) {
+
+            }
+
+            for (Developer developer : eachScrum.developers) {
+
+            }
+
+        }
+        1. total budget
+        2. total Number of testers in Bank of America
+        3. total number of developers in Bank of America
+        4. what's the max & min salaries of the tester
+        5. what's the max & min salaries of the developers
+    */
+
 
     }
 
