9867210c369bd59e41926908511e2dbd89b1b2dd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-09 12:59:54 -0400

    day41

--- src/day41_CustomClass_Constructor/Employee.java
+++ src/day41_CustomClass_Constructor/Employee.java
@@ -2,5 +2,21 @@ package day41_CustomClass_Constructor;
 
 public class Employee {
 
+    public String name, jobTitle;
+    public double salary;
+
+    public Employee(String name, String jobTitle, double salary){
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 
 }
---
+++ src/day41_CustomClass_Constructor/EmployeeObjects.java
@@ -0,0 +1,19 @@
+package day41_CustomClass_Constructor;
+
+public class EmployeeObjects {
+
+    public static void main(String[] args) {
+
+       Employee  employee1=new Employee("Sabir", "SDET", 110000);
+
+       Employee employee2 = new Employee("Maria", "SM", 115000);
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+
+
+    }
+
+
+
+}
