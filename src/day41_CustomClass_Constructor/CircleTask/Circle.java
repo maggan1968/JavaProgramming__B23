c74d6f319de29b0dbde4287c6fa0e720e522092b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-09 16:31:09 -0400

    day41

---
+++ src/day41_CustomClass_Constructor/CircleTask/Circle.java
@@ -0,0 +1,36 @@
+package day41_CustomClass_Constructor.CircleTask;
+
+public class Circle {
+
+    public double radius;
+    public double diameter;
+
+    public static double PI;
+
+    static{
+        PI = 3.14;
+    }
+
+    public  Circle(double radius){
+        this.radius = radius;
+        diameter = 2 * radius;
+    }
+
+    public double area(){
+        return radius* radius * PI;
+    }
+
+    public double perimeter(){
+        return diameter * PI;
+    }
+
+
+    public String toString() {
+        return "Circle{" +
+                "radius= " + radius +
+                ", diameter= " + diameter +
+                ", area= " + area() +
+                ", perimeter= " + perimeter() +
+                '}';
+    }
+}
