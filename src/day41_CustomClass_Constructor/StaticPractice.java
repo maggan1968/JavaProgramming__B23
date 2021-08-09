eee047b7e64734fe04387ed84c54be64fde3af58
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-09 10:28:58 -0400

    day41

---
+++ src/day41_CustomClass_Constructor/StaticPractice.java
@@ -0,0 +1,29 @@
+package day41_CustomClass_Constructor;
+
+public class StaticPractice {
+
+    static class Test{
+         static int b;
+    }
+
+
+    public static int a =100;
+
+    public static void method1(){
+
+    }
+
+    static{
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StaticPractice.Test.b);
+
+        StaticPractice obj = new StaticPractice();
+       // System.out.println( new StaticPractice.TestB. );
+
+    }
+
+
+}
