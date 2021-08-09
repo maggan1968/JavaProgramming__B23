e1d589eb23de101ff2bb0e294e58e533d1a39d33
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-09 16:34:23 -0400

    day41

---
+++ src/day41_CustomClass_Constructor/CircleTask/CircleObjects.java
@@ -0,0 +1,18 @@
+package day41_CustomClass_Constructor.CircleTask;
+
+public class CircleObjects {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(5);
+
+        System.out.println(circle1);
+
+        Circle circle2 = new Circle(7.5);
+
+        System.out.println(circle2);
+
+
+    }
+
+}
