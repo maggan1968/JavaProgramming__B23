ab127bb47323456da33f45ca2fb603b92e38d32d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-10 11:40:19 -0400

    day42

---
+++ src/day42_CustomClass_Constructors/warmupTasks/ItemObjects.java
@@ -0,0 +1,18 @@
+package day42_CustomClass_Constructors.warmupTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ItemObjects {
+
+    public static void main(String[] args) {
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.addAll(Arrays.asList(
+               // new
+        ));
+
+
+    }
+
+}
