579200fe655b5371402d39fa2f0c087676e89b2c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-10 12:12:51 -0400

    day42

---
+++ src/day42_CustomClass_Constructors/warmupTasks/CybertekStudentObjects.java
@@ -0,0 +1,26 @@
+package day42_CustomClass_Constructors.warmupTasks;
+
+public class CybertekStudentObjects {
+
+    public static void main(String[] args) {
+
+        CybertekStudent student1 = new CybertekStudent("Feruza", 20, 'F');
+        CybertekStudent student2 = new CybertekStudent("Alisa", 27, 'F');
+
+        System.out.println(student1);
+        System.out.println(student2);
+
+        student1.attendClass();
+        student2.replit();
+
+        student1.askForBreak();
+
+
+
+
+
+    }
+
+
+
+}
