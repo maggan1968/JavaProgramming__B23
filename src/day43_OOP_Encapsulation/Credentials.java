a677df46d0e0e073f775d836e3582b5fe34309e2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-12 16:17:30 -0400

    day43

--- src/day43_OOP_Encapsulation/Credentials.java
+++ src/day43_OOP_Encapsulation/Credentials.java
@@ -4,6 +4,11 @@ public class Credentials {
 
     private String username, password;
 
+    public Credentials(String username, String password) {
+       setUsername(username);
+       setPassword(password);
+    }
+
     public String getUsername() {
         return username;
     }
