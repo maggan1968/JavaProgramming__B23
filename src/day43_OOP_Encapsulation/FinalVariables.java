c653e8fb74af0b558bd6528c9f5ccb2c43f2ce86
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-12 17:00:21 -0400

    day43

--- src/day43_OOP_Encapsulation/FinalVariables.java
+++ src/day43_OOP_Encapsulation/FinalVariables.java
@@ -1,4 +1,31 @@
 package day43_OOP_Encapsulation;
 
+import java.time.LocalDate;
+
 public class FinalVariables {
+
+    private  final String gender ;
+    private final LocalDate DOB;
+    private double salary;
+
+    public FinalVariables(String gender, LocalDate DOB) {
+        this.gender = gender;
+        this.DOB = DOB;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public static void main(String[] args) {
+        final int a = 100;
+           //     a = 200;
+        System.out.println(a);
+
+
+
+
+
+    }
+
 }
