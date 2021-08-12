a1b5e6961bdac261a0309d99e7749d2c360bbb76
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-12 15:17:35 -0400

    day43

---
+++ src/day43_OOP_Encapsulation/Rectangle.java
@@ -0,0 +1,47 @@
+package day43_OOP_Encapsulation;
+
+public class Rectangle {
+
+    private double length, width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length < 0){
+            return;
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if( width < 0){
+            return;
+        }
+        this.width = width;
+    }
+
+    public double area(){
+        return length * width;
+    }
+
+    public double perimeter(){
+        return  2*(length+width);
+    }
+
+    public boolean equals(Rectangle rectangle){
+        return perimeter() == rectangle.perimeter();
+    }
+
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                '}';
+    }
+}
