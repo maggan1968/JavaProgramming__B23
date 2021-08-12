bcb6120e060f0860bb7fe3581562d5eb4f4452d4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-12 14:52:28 -0400

    day43

--- src/day43_OOP_Encapsulation/Circle.java
+++ src/day43_OOP_Encapsulation/Circle.java
@@ -1,7 +1,7 @@
 package day43_OOP_Encapsulation;
 
 public class Circle {
-    //               10      30
+    //               5     20
     private double radius, diameter;
 
     private static double PI = 3.14;
@@ -16,15 +16,19 @@ public class Circle {
 
     public void setRadius(double radius){
         if(radius < 0){
-            System.out.println("Radius of the circle can not be negative");
-            return; // exits the method
+            System.out.println("Radius can not be negative");
+            return;
         }
-
         this.radius  =radius; // r= 3
-        setDiameter(radius* 2); // d= r* 2
+       setDiameter(radius * 2);
+                    //10
     }
 
     public void setDiameter(double diameter){
+        if(diameter < 0){
+            System.out.println("Diameter of the circle cannot be negative");
+            return;
+        }
         this.diameter = diameter;
         setRadius(diameter/2);
     }
@@ -34,5 +38,40 @@ public class Circle {
        setRadius(radius);
     }
 
+    public double area(){
+        return radius * radius * PI;
+    }
 
+    public double perimeter(){
+        return diameter * PI;
+    }
+
+    public boolean equals(Circle circle){
+        return radius == circle.radius;
+    }
+
+
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", diameter=" + diameter +
+                '}';
+    }
 }
+
+/*
+1. Create a class named Circle
+			Private variables:
+				radius, diameter, PI
+
+			generate getter & setter for all private fileds
+					Make sure that radius & diameter of circle can never be set to negative
+
+			add a constructor that can set all the fields
+
+			Extra methods:
+				equal(): accepts a Circle Object and returns true if the given circle is equal to the current circle
+				calcArea(): returns the area
+				calcPerimeter(): returns the perimeter
+				toString()
+ */
