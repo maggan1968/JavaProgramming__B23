af7cf982edf8fda1e59376d2dda418a646cc199b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-12 11:22:08 -0400

    day43

--- src/utilities/Data.java
+++ src/utilities/Data.java
@@ -1,19 +1,24 @@
 package utilities;
 
+import day43_OOP_Encapsulation.AccessModifiers;
+
 public class Data {
 
     public String str1, str2;
-
     public void method1(){}
-
     public void method2(){}
-
-
     public static String str3, str4,str5;
-
     public static void method3(){}
-
     public static void method4(){}
+
+
+    public static void main(String[] args) {
+
+        System.out.println(AccessModifiers.publicVariable); // public access modifier is reachable out the package
+      //  System.out.println(AccessModifiers.defaultVariable); // default access modifier is NOT reachable out the package
+      //  System.out.println(AccessModifiers.privateVariable); // private access modifier is NOT reachable out the package
+
+    }
 
 
 }
