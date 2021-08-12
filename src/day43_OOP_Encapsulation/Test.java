c004d0b017ed4ebe538bb2f91a0c657cec66deb8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-12 11:14:27 -0400

    day43

---
+++ src/day43_OOP_Encapsulation/Test.java
@@ -0,0 +1,15 @@
+package day43_OOP_Encapsulation;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println( AccessModifiers.publicVariable ); //public access modifier is reachable within the same package
+        System.out.println(AccessModifiers.defaultVariable); //default access modifier is reachable within the same package
+   //     System.out.println(AccessModifiers.privateVariable); //private access modifier is not reachable within the same package
+
+
+
+    }
+
+}
