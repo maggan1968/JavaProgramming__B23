2a039a6718f213b33c7a565d6072cdf48d82f84f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-13 11:44:01 -0400

    day44

--- src/day44_Recap/personTask/Person.java
+++ src/day44_Recap/personTask/Person.java
@@ -1,8 +1,64 @@
 package day44_Recap.personTask;
 
+import java.time.LocalDate;
+
 public class Person {
 
+    private String name;
+    private char gender;
+    private final int age ;
+    private final LocalDate dateOfBirth;
+    private final String countryOfBirth;
+    private final String language;
 
+    public Person(String name, char gender, LocalDate dateOfBirth, String countryOfBirth, String language) {
+        setName(name);
+        setGender(gender);
+        if(dateOfBirth.isBefore(LocalDate.now())) {
+            this.dateOfBirth = dateOfBirth;
+        }else{
+            this.dateOfBirth = null;
+        } // age cannot be negative
+
+        this.countryOfBirth = countryOfBirth;
+        this.language = language;
+        age = LocalDate.now().getYear() - this.dateOfBirth.getYear();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getCountryOfBirth() {
+        return countryOfBirth;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(char gender) {
+        if(gender != 'M' || gender != 'F' || gender != 'm' || gender != 'f' ){ // if gender is not male or female
+            return; // exits the method
+        }
+        this.gender = gender;
+    }
 
 
 }
