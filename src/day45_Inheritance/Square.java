d2507034e3af3e5e8bafa75ad331bdf4559050d5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-16 10:42:19 -0400

    day45

---
+++ src/day45_Inheritance/Square.java
@@ -0,0 +1,56 @@
+package day45_Inheritance;
+
+public class Square {
+
+    private double side;
+
+    public Square(double side) {
+       setSide(side);
+    }
+
+    public double getSide(){
+       return side;
+    }
+
+    public void setSide(double side){
+        if(side < 0){
+            return;
+        }
+        this.side = side;
+    }
+
+    public double area(){
+        return side * side ;
+    }
+
+    public double perimeter(){
+        return 4 * side;
+    }
+
+    public boolean equals(Square square){
+        return side == square.side;
+    }
+
+    public String toString() {
+        return "Square{" +
+                "side= " + side +
+                "area= " + area() +
+                "perimeter= " + perimeter() +
+                '}';
+    }
+
+}
+
+/*
+1. create a class named Square
+            private variables:
+                side (double)
+
+            Encapsulate All the private fields
+                (side can not be set to negative)
+
+            Add a constructor that can set the fields
+
+            Extra methods:
+                area(), perimeter(), toString(), equals()
+ */
