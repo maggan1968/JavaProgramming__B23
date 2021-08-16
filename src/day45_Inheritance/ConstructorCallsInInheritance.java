745d763926a7170b28942904d9ef78ba2a7627d4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-16 15:22:48 -0400

    day45

---
+++ src/day45_Inheritance/ConstructorCallsInInheritance.java
@@ -0,0 +1,25 @@
+package day45_Inheritance;
+
+class A{
+    public A(){
+        System.out.println("A");
+    }
+}
+
+class B extends A{
+
+    public B(){
+        //  super(); //A
+        System.out.println("B");
+    }
+
+}
+
+public class ConstructorCallsInInheritance {
+
+    public static void main(String[] args) {
+        new B();
+
+    }
+
+}
