399ec07702b8b23e1491cf7e629a3805865e8a8c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-16 16:02:53 -0400

    day45

---
+++ src/day45_Inheritance/employeeTask/Person.java
@@ -0,0 +1,28 @@
+package day45_Inheritance.employeeTask;
+
+public class Person {
+
+    public String name;
+    public final char gender;
+    public int age;
+
+    public Person(String name, char gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public void eat(String food){
+        System.out.println(name +" is eating "+food);
+    }
+
+    public void drink(String drink){
+        System.out.println(name +" is drinking "+drink);
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+
+}
