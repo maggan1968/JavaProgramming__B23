0a568d7df4460a504851158f5390bceb2b359b7a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-16 16:32:56 -0400

    day45

---
+++ src/day45_Inheritance/employeeTask/Driver.java
@@ -0,0 +1,13 @@
+package day45_Inheritance.employeeTask;
+
+public class Driver extends Employee {
+    public Driver(String name, char gender, int age, long id, String jobTitle, double salary) {
+        super(name, gender, age, id, jobTitle, salary);
+    }
+
+    public void drive(){
+        System.out.println(name+" is driving");
+    }
+
+
+}
