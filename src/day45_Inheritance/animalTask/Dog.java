a356b2ca320a4d46e5e45fe9fa61e24e3bd9eada
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-16 15:31:58 -0400

    day45

--- src/day45_Inheritance/animalTask/Dog.java
+++ src/day45_Inheritance/animalTask/Dog.java
@@ -1,14 +1,14 @@
 package day45_Inheritance.animalTask;
 
 public class Dog extends Animal  { // Dog Is An animal
+
+    public Dog(String name, String breed, char gender, String size, int age, String color) {
+        super(name, breed, gender, size, age, color);
+    } // Dog Is An animal
 /*
 name, breed, gender, size, age, color, sleeps, breath
 setInfo(), eat(), drink(), sleep(), toString()
  */
-
-    public Dog(String name, String breed, char gender, String size, int age, String color){
-       setInfo(name, breed, gender, size, age, color);
-    }
 
     public void bark(){
         System.out.println(name+" is barking");
