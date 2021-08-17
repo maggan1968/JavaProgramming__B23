e1fd23aaa52d8df5376488b22025ed375b47c26b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-17 15:58:39 -0400

    day46

--- src/day46_Inheritance/shapeTask/Rectangle.java
+++ src/day46_Inheritance/shapeTask/Rectangle.java
@@ -2,6 +2,30 @@ package day46_Inheritance.shapeTask;
 
 public class Rectangle {
 
+    private double length, width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length < 0){
+            return;
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width <0){
+            return;
+        }
+        this.width = width;
+    }
+
 
 
 }
