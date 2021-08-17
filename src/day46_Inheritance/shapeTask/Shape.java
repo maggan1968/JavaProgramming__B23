395f3ed7c5bd568423a3084e33768adc0f1881c2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-17 14:52:03 -0400

    day46

---
+++ src/day46_Inheritance/shapeTask/Shape.java
@@ -0,0 +1,45 @@
+package day46_Inheritance.shapeTask;
+
+public class Shape {
+
+    public final String name;
+    public final static boolean isShape, hasArea, hasPerimeter;
+
+    static{
+        isShape = true;
+        hasArea = true;
+        hasPerimeter = true;
+    }
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name= '" + name + '\'' +
+                "area= " + area()  +
+                "perimeter= " + perimeter()  +
+                '}';
+    }
+
+
+}
+/*
+1. create a class called Shape
+						variables: name, isShape(boolean), hasArea(boolean), hasPerimeter(boolean)
+
+						add a constructor that can set all the fields
+
+						methods: calcArea(), calcPerimeter(),
+								 toString(): returns the name, area and perimeter that are returned by calcArea(), calcPerimeter()
+ */
