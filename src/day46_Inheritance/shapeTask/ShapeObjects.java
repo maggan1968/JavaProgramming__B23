542044ff9ea08906de48fc1d32cf7235105558d8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-17 16:26:44 -0400

    day46

---
+++ src/day46_Inheritance/shapeTask/ShapeObjects.java
@@ -0,0 +1,15 @@
+package day46_Inheritance.shapeTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(3);
+        System.out.println(circle.area());
+        System.out.println(circle.perimeter());
+
+
+
+    }
+
+}
