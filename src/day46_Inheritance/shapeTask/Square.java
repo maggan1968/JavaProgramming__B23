0459a0e56c6fb6e80ba30e735f493bf075842d06
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-17 16:22:08 -0400

    day46

---
+++ src/day46_Inheritance/shapeTask/Square.java
@@ -0,0 +1,32 @@
+package day46_Inheritance.shapeTask;
+
+public class Square extends Shape {
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side < 0){
+            return;
+        }
+        this.side = side;
+    }
+
+    public Square(double side){
+        super("Square");
+        setSide(side);
+    }
+
+    public double area() {
+        return side * side;
+    }
+
+    public double perimeter() {
+        return side * 4;
+    }
+
+
+}
