d2a4ee595c07906302bf0ffcadb63204c43d1945
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-17 10:28:29 -0400

    day46

---
+++ src/day46_Inheritance/carTask/Car.java
@@ -0,0 +1,42 @@
+package day46_Inheritance.carTask;
+
+public class Car {
+
+    public String brand, model;
+    public final int year;
+    public double price;
+    public String color;
+    public long miles;
+
+    public Car(String brand, String model, int year, double price, String color, long miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println(brand+" "+model+" is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand+" "+model+" is ready to drive");
+    }
+
+
+
+
+
+}
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+ */
