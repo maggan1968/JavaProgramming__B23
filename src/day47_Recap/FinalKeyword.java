c3bb836443581cb353ea6a6e530a6de830e6ad4f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-19 10:26:19 -0400

    day47

---
+++ src/day47_Recap/FinalKeyword.java
@@ -0,0 +1,29 @@
+package day47_Recap;
+
+public class FinalKeyword {
+
+    public final void sleep(){
+        System.out.println("Sleep");
+    }
+
+    public void area(){
+
+    }
+
+
+
+    public final static void main(String[] args) {
+
+    }
+
+   protected final static void main(Integer[] args) {
+
+    }
+
+    final static void main(Double[] args) {
+
+    }
+
+
+
+}
