ba49d252018f43288cd18a8066049f701f24931f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-19 10:55:55 -0400

    day47

---
+++ src/day47_Recap/browsersTask/Browser.java
@@ -0,0 +1,45 @@
+package day47_Recap.browsersTask;
+
+public class Browser {
+
+    public void open(){
+        System.out.println("Opening Chrome Browser");
+    }
+
+    public void close(){
+        System.out.println("Closing Chrome Browser");
+    }
+
+    public final void navigate(String url){
+        System.out.println("Type: "+url);
+        System.out.println("Press Enter");
+    }
+
+    public final void refresh(){
+        System.out.println("F5");
+    }
+
+    public final void backward(){
+        System.out.println("backward");
+    }
+
+    public final void forward(){
+        System.out.println("forward");
+    }
+
+
+
+}
+
+/*
+open()
+close()
+navigate()
+refresh()
+
+
+chrome1.navigate("google.com")
+chrome2.navigate("facebook")
+
+chrome1.refresh()
+ */
