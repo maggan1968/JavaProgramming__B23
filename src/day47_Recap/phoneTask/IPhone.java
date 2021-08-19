7e36eb23d11f28c89787ca83c48abc6ae5ede67d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-19 15:09:27 -0400

    day47

---
+++ src/day47_Recap/phoneTask/IPhone.java
@@ -0,0 +1,29 @@
+package day47_Recap.phoneTask;
+
+public class IPhone extends Phone {
+
+    public IPhone( String model, String size, String color, double price) {
+        super("Apple", model, size, color, price);
+    }
+
+    public void call(long phoneNumber){
+        System.out.println("Apple' iphone is calling "+phoneNumber);
+    }
+
+    public void call(long phone1, long phone2){
+        call(phone1);
+        call(phone2);
+    }
+
+    public void call(long phone1, long phone2, long phone3){
+        call(phone1, phone2);
+        call(phone3);
+    }
+
+    @Override
+    public void text(long phoneNumber) {
+        System.out.println("Apple' iphone is texting to "+phoneNumber);
+    }
+
+
+}
