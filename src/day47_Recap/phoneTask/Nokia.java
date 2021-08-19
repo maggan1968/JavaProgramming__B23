c7cf9d088ae6aa38143e6729781bc09c7d056c8a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-19 15:52:48 -0400

    day47

---
+++ src/day47_Recap/phoneTask/Nokia.java
@@ -0,0 +1,13 @@
+package day47_Recap.phoneTask;
+
+public class Nokia extends Phone {
+
+    public Nokia( String model, String size, String color, double price) {
+        super("Nokia", model, size, color, price);
+    }
+
+    public void selfDefense(){
+        System.out.println(getBrand()+" "+getModel()+" is designed for self defense");
+    }
+
+}
