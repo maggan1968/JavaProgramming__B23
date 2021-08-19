d90884dc088d98450d1175f9833f3d86d603acd2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-19 15:28:18 -0400

    day47

--- src/day47_Recap/phoneTask/IPhone.java
+++ src/day47_Recap/phoneTask/IPhone.java
@@ -1,6 +1,6 @@
 package day47_Recap.phoneTask;
 
-public class IPhone extends Phone {
+public final class IPhone extends Phone {
 
     public IPhone( String model, String size, String color, double price) {
         super("Apple", model, size, color, price);
@@ -25,7 +25,7 @@ public class IPhone extends Phone {
         System.out.println("Apple' iphone is texting to "+phoneNumber);
     }
 
-    public void faceTime(long phoneNumber){
+    public final void faceTime(long phoneNumber){
         System.out.println("Apple's iphone is face timing with "+phoneNumber);
     }
 
