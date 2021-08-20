441cdafc21603007bee4a1c5819b33c2c470438e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-20 11:43:03 -0400

    day48

---
+++ src/day48_Recap/cybertekTask/Tester.java
@@ -0,0 +1,30 @@
+package day48_Recap.cybertekTask;
+
+import java.time.LocalDate;
+
+public final class Tester extends Employee{
+
+
+    public Tester(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary) {
+        super(name, gender, dOB, employeeId, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is testing");
+    }
+
+    @Override
+    public void attendMeeting() {
+        System.out.println(getJobTitle()+" "+getName()+" is attending scrum meeting");
+    }
+
+
+}
+
+/*
+1. Tester
+        variables: name, gender, age, dateOfBirth, jobTitle, employeeID, salary
+
+        methods: work(), attendMeeting()
+ */
