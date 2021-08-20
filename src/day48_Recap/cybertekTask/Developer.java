c63335b388abdb4e28ca8eb594bf0cc0273a9007
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-20 11:47:21 -0400

    day48

---
+++ src/day48_Recap/cybertekTask/Developer.java
@@ -0,0 +1,28 @@
+package day48_Recap.cybertekTask;
+
+import java.time.LocalDate;
+
+public final class Developer extends Employee {
+
+
+    public Developer(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary) {
+        super(name, gender, dOB, employeeId, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is coding");
+    }
+
+    @Override
+    public void attendMeeting() {
+        System.out.println(getJobTitle()+" "+getName()+" is attending grooming meeting");
+    }
+}
+
+/*
+2. Developer
+            variables: name, gender, age, dateOfBirth, jobTitle, employeeID, salary
+
+            methods: work(), attendMeeting()
+ */
