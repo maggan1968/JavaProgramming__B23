f5cf12de159600ae8329d220b120d98e60a42c94
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-20 10:50:49 -0400

    day48

---
+++ src/day48_Recap/cybertekTask/Person.java
@@ -0,0 +1,44 @@
+package day48_Recap.cybertekTask;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String name;
+    private final char gender;
+    private final int age;
+    private final LocalDate dOB;
+
+    public Person(String name, char gender, LocalDate dOB) {
+        setName(name);
+        this.gender = gender;
+        this.dOB = dOB;
+        age = LocalDate.now().getYear() - this.dOB.getYear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDate getdOB() {
+        return dOB;
+    }
+}
+
+/*
+1. create a class named Person
+        variables: name, gender, age, dateOfBirth
+        methods: eat(), sleep(), drink()
+ */
