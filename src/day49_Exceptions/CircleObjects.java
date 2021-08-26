e870d4862382a05998c3df8383463235dde79e4f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-26 16:21:28 -0400

    day49

---
+++ src/day49_Exceptions/CircleObjects.java
@@ -0,0 +1,15 @@
+package day49_Exceptions;
+
+public class CircleObjects {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(10);
+
+        System.out.println(circle1);
+
+
+
+    }
+
+}
