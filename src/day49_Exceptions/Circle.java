ab8390f9296acb4809103d00048d375601eac706
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-26 16:17:41 -0400

    day49

---
+++ src/day49_Exceptions/Circle.java
@@ -0,0 +1,30 @@
+package day49_Exceptions;
+
+public class Circle {
+
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius < 0){
+            throw new RuntimeException("Radius of the circle cannot be negative");
+        }
+        this.radius = radius;
+    }
+
+    public Circle(double radius) {
+        setRadius(radius);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+
+
+}
