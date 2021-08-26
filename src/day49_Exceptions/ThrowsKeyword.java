d19387d348001a1cb8183b065c4b730578e4e28d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-26 15:19:26 -0400

    day49

--- src/day49_Exceptions/ThrowsKeyword.java
+++ src/day49_Exceptions/ThrowsKeyword.java
@@ -2,29 +2,43 @@ package day49_Exceptions;
 
 public class ThrowsKeyword {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException{
 
         System.out.println("test started");
         System.out.println("Step 1");
-
-            Thread.sleep(3000);
+        sleep(2.5);
 
        // System.out.println( 9 / 0);
 
         System.out.println("Step 2");
+        sleep(2.5);
 
-            Thread.sleep(3000);
     //    System.out.println( "".charAt(-200));
-
         System.out.println("test completed");
 
     }
 
 
-    public static void sleep(){
+    public static void sleep(double seconds) throws InterruptedException {
+
+        long milliSeconds = (long)(seconds * 1000);
+        Thread.sleep(milliSeconds);
+    }
+
+
+    /*
+    public static void sleep(double seconds)  {
+
+        long milliSeconds = (long)(seconds * 1000);
+        try {
+            Thread.sleep(milliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
+     */
 
 
 }
