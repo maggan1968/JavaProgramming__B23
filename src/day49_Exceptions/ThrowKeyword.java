b1b79f03a6a04559b5c48a51b17e2c9581e683ab
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-26 16:13:04 -0400

    day49

--- src/day49_Exceptions/ThrowKeyword.java
+++ src/day49_Exceptions/ThrowKeyword.java
@@ -4,6 +4,26 @@ public class ThrowKeyword {
 
     public static void main(String[] args) {
 
+      //  throw new RuntimeException("This is an unexpected event");
+
+        //System.out.println("");
+
+       String browser = "chrome";
+
+       //chrome, firefox, opera, safari
+
+        if(browser.equalsIgnoreCase("chrome")){
+            System.out.println("Chrome browser is selected");
+        }else if(browser.equalsIgnoreCase("firefox")){
+            System.out.println("Firefox browser is selected");
+        }else if(browser.equalsIgnoreCase("opera")){
+            System.out.println("Opera browser is selected");
+        }else if(browser.equalsIgnoreCase("safari")){
+            System.out.println("safari browser is selected");
+        }else{
+            throw new RuntimeException("Invalid Browser name: "+browser);
+        }
+
 
 
 
