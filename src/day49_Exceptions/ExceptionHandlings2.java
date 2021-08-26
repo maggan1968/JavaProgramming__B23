211e22fe63de9041845b4140fdfd540267a72346
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-26 13:05:49 -0400

    day49

---
+++ src/day49_Exceptions/ExceptionHandlings2.java
@@ -0,0 +1,56 @@
+package day49_Exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.UnknownHostException;
+
+public class ExceptionHandlings2 {
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream file = new FileInputStream("");
+        }catch (FileNotFoundException e){
+            e.printStackTrace(); // gives the full report of the exceptions,
+        }
+
+
+        System.out.println("Test Completed");
+
+        System.out.println("---------------------------------------------");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Test Completed");
+
+
+        System.out.println("---------------------------------------------");
+
+        try {
+            System.out.println(100 / 0);
+            System.out.println("try block");
+        }catch (RuntimeException e){
+            System.out.println("Catch block");
+        }finally { // gets executed even if the exception is not handled
+            System.out.println("Finally block");
+        }
+
+
+        System.out.println("Test Completed");
+
+        System.out.println("------------------------------------");
+
+
+
+
+
+
+
+    }
+
+
+}
