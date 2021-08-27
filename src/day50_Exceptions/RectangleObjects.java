99c194333fadfca291cee333628a1ee056be4dc3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-27 12:07:47 -0400

    day50

--- src/day50_Exceptions/RectangleObjects.java
+++ src/day50_Exceptions/RectangleObjects.java
@@ -2,13 +2,27 @@ package day50_Exceptions;
 
 public class RectangleObjects {
 
-    public static void main(String[] args) {
+    public void method() throws Throwable {
 
-        Rectangle r1 = new Rectangle(5, -4);
+        this.finalize(); // clean up
+    }
+
+    public final static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(5, 4);
 
         System.out.println(r1);
 
+        String str = "Java";
+        str = "Python";
+
+        System.out.println(str);
+
 
     }
+
+
+
+
 
 }
