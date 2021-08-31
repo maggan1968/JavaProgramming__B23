27062980d3ea4c09f342512164a88bbf69481118
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-31 11:32:04 -0400

    day51

---
+++ src/day51_Abstraction/shapeTask/ShapeObjects.java
@@ -0,0 +1,14 @@
+package day51_Abstraction.shapeTask;
+
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+    //    Shape shape = new Shape("Shape");
+
+
+    }
+
+
+
+}
