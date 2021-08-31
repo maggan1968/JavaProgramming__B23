c054c13ddcaae3623b8257566a4154ddf2fc6d0b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-31 14:53:07 -0400

    day51

---
+++ src/day51_Abstraction/carTask/BMW.java
@@ -0,0 +1,18 @@
+package day51_Abstraction.carTask;
+
+public class BMW extends Car{
+
+
+    public BMW( String model, String color, int year, double price) {
+        super("BMW", model, color, year, price);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("--------------------------");
+        System.out.println("Call mechanic");
+        System.out.println("Oil change");
+        System.out.println("Jump start");
+        System.out.println("--------------------------");
+    }
+}
