060167b7b7bbc0c4cef7711560e1318504fb5334
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-31 11:44:17 -0400

    day51

--- src/day51_Abstraction/shapeTask/Circle.java
+++ src/day51_Abstraction/shapeTask/Circle.java
@@ -2,5 +2,19 @@ package day51_Abstraction.shapeTask;
 
 public class Circle {
 
+    private double r;
+    public final static double PI = 3.14;
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+
+
+
 
 }
