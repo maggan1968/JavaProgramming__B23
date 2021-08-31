4e8c76bd4e864382883b9b4950311ed44f1ad778
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-31 14:49:38 -0400

    day51

---
+++ src/day51_Abstraction/carTask/Tesla.java
@@ -0,0 +1,23 @@
+package day51_Abstraction.carTask;
+
+public class Tesla extends Car{
+
+
+    public Tesla( String model, String color, int year, double price) {
+        super("Tesla", model, color, year, price);
+    }
+
+    public void autoPilot(){
+        System.out.println("Tesla "+getModel()+" is self driving");
+    }
+
+
+    @Override
+    public void start() {
+        System.out.println("------------------------------------");
+        System.out.println("Say \"Start\"");
+        System.out.println("------------------------------------");
+    }
+
+
+}
