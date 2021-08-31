3b71233929c0b7e4145bed71c4ae9b55d19278ce
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-31 12:04:59 -0400

    day51

--- src/day51_Abstraction/shapeTask/Rectangle.java
+++ src/day51_Abstraction/shapeTask/Rectangle.java
@@ -2,6 +2,31 @@ package day51_Abstraction.shapeTask;
 
 public class Rectangle {
 
+    private double l, w;
+
+    public double getL() {
+        return l;
+    }
+
+    public void setL(double l) {
+        if(l <= 0){
+            throw new RuntimeException("Invalid Length: "+l);
+        }
+        this.l = l;
+    }
+
+    public double getW() {
+        return w;
+    }
+
+    public void setW(double w) {
+        if(w <= 0){
+            throw new RuntimeException("Invalid Width: "+w);
+        }
+        this.w = w;
+    }
+
+
 
 
 }
