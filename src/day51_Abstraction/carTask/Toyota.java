7c4a33af1737ff5b0e892f658db06815f3cbedd5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-08-31 14:41:34 -0400

    day51

--- src/day51_Abstraction/carTask/Toyota.java
+++ src/day51_Abstraction/carTask/Toyota.java
@@ -3,13 +3,16 @@ package day51_Abstraction.carTask;
 public final class Toyota extends Car{
 
 
-    public Toyota(String make, String model, String color, int year, double price) {
-        super(make, model, color, year, price);
+    public Toyota( String model, String color, int year, double price) {
+        super("Toyota", model, color, year, price);
     }
 
     @Override
     public void start() {
-
+        System.out.println("------------------------------");
+        System.out.println("Insert the key");
+        System.out.println("Twist the key to ignition");
+        System.out.println("------------------------------");
     }
 
 }
